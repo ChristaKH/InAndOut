608271e70e4ad0aed3ba40b0bf5e6c9d2a580dfe
Christa Hatch <christahatch1994@pointloma.edu>
2019-02-20 11:30:27 -0800

    Finish xml for order summary activity Connect variables in java file to views in xml file Begin retrieving intent

--- app/src/main/java/chatch/cs134/miracosta/inandout/OrderActivity.java
+++ app/src/main/java/chatch/cs134/miracosta/inandout/OrderActivity.java
@@ -1,13 +1,36 @@
 package chatch.cs134.miracosta.inandout;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+
+    TextView orderTotalTextView;
+    TextView itemsOrderedTextView;
+    TextView subtotalTextView;
+    TextView taxTextView;
+    Button startNewOrderButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        // Connect variables in this class to views in XML
+        orderTotalTextView = findViewById(R.id.orderTotalTextView);
+        itemsOrderedTextView = findViewById(R.id.itemsOrderedTextView);
+        subtotalTextView = findViewById(R.id.subtotalTextView);
+        taxTextView = findViewById(R.id.taxTextView);
+        startNewOrderButton = findViewById(R.id.startNewOrderButton);
+
+        // Take in other intent
+        Intent orderIntent = getIntent();
+
+        // Take info from intent: order total, number of items in order, subtotal,
+        // and tax
+        double orderTotal = orderIntent.getDoubleExtra("Total", 0.00);
     }
 }
